823fe4016e75e601a9f274d4873598247fc4fe14
nguyenthanhlinh <thanhlinh12314@gmail.com>
2018-04-19 11:41:24 +0700

    class cicle and class cylinder

---
+++ Cylinder.java
@@ -0,0 +1,34 @@
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder() {
+    }
+
+    public Cylinder(double height) {
+        this.height = height;
+    }
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getVolume(double radius, double height){
+        return Math.PI * radius * radius * height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                " height = " + this.height +
+                " has volume = " + getVolume(getRadius(), this.height);
+    }
+}
---
+++ CylinderTest.java
@@ -0,0 +1,10 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        double radius = 3;
+        String color = "Blue";
+        double height = 5;
+
+        Cylinder cylinder = new Cylinder(radius, color, height);
+        System.out.println("Cylinder " + cylinder);
+    }
+}
